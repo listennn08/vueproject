164fa127a18e4e218f731e84886f41c7ccc56b8c
MattTai <listennn08@outlook.com>
2020-01-16 14:54:18 +0800

    fix

--- src/pages/Taiwan.vue
+++ src/pages/Taiwan.vue
@@ -118,7 +118,8 @@ export default {
                         .attr('width', width)
                         .attr('height', height*2)
                         .attr('viewBox', `0 0 ${width} ${height}`)
-      var url = '../vueproject/static/COUNTY_MOI_1080726.json'
+      var url = '/vueproject/static/COUNTY_MOI_1080726.json'
+      import url from ''
       await d3.json(url)
               .then((geometry) => {
                 // console.log(geometry)
