9b10f3ebc4bfca25f7472dd5eedd3b368293747a
MattTai <listennn08@outlook.com>
2020-03-19 12:57:26 +0800

    upload

--- src/pages/rateConvert.vue
+++ src/pages/rateConvert.vue
@@ -4,19 +4,19 @@
         <div class="col-md-5 mt-5 offset-md-3">
             <div class="row">
                 <div class="form-group">
-                    <select id="From" class="custom-select">
+                    <select id="From" class="custom-select" @change="changeCurrency()">
                         <option :value="item.CurrencyCode" v-for="(item, index) in options" :key="index">{{ item.CurrencyCnName }}</option>
                     </select>
                     <button id="changebtn" class="btn btn-outline-success offset-5">↑↓</button>
-                    <select id="To" class="custom-select">
+                    <select id="To" class="custom-select" @change="changeCurrency()">
                         <option :value="item.CurrencyCode" v-for="(item, index) in options" :key="index">{{ item.CurrencyCnName }}</option>
                     </select>
                 </div>
             </div>
             <div class="row">
                 <div class="form-inline mb-2 col-md-5 offset-2">
-                    <input id="FromNum" class="form-control mr-5" type="number" :value="fromNum"> 
-                    <input id="ToNum" class="form-control" disabled="disabled" type="number" v-model="toNum">
+                    <input id="FromNum" class="form-control mr-5" type="number" :placeholder="fromNum" v-model="fromNum" min="0">
+                    <input id="ToNum" class="form-control" disabled="disabled" type="number" :value="toNum">
                 </div>
             </div>
             <div class="row">
@@ -32,35 +32,44 @@ export default {
         return {
             options: '',
             fromNum: 0,
+            toNum: 0,
             API: "https://cors-anywhere.herokuapp.com/https://tw.rter.info/capi.php"
-            
+
         }
     },
     created () {
         axios.get('../static/currencies.json')//.get('/vueproject/static/currencies.json')
             .then(async response => this.options = await response.data)
     },
-    computed: {
-        /*
-        toNum () {
-            let from = document.getElementById('From').options[document.getElementById('From').selectedIndex].value;
-            let To = document.getElementById('To').options[document.getElementById('To').selectedIndex].value;
-            let tmpTo, rate, fromN;
-            if (from != "USD") {
-                tmpTo = from;
-                from = "USD"
-                rate = this.getRate(from, tmpTo);
-                console.log(rate)
-                fromN = parseInt(this.fromNum) / parseFloat(rate);
-            }
-            tmpTo = To;
-            console.log(from+" " + tmpTo)
-            rate = this.getRate(from, tmpTo) // .then(async data=>await data);
-            return parseFloat(fromN) * parseFloat(rate);
-        }
-        */
+    watch: {
+      fromNum: {
+        async handler (val) {
+          this.changeCurrency(val);
+        },
+        deep: true
+      }
     },
     methods: {
+        async changeCurrency (val) {
+            this.fromNum = this.fromNum ? parseFloat(this.fromNum) : 0;
+            val = val ? val : this.fromNum;
+            let from = document.getElementById('From').options[document.getElementById('From').selectedIndex].value;
+            let To = document.getElementById('To').options[document.getElementById('To').selectedIndex == -1 ? 0 : document.getElementById('To').selectedIndex].value;
+            let tmpTo, rate, fromN;
+            if (from == To) {
+              this.toNum = this.fromNum;
+              return;
+            }
+            if (from != "USD") {
+              tmpTo = from;
+              from = "USD"
+              rate = await this.getRate(from, tmpTo);
+              fromN = parseFloat(val) / parseFloat(rate);
+            }
+            tmpTo = To;
+            rate = await this.getRate(from, tmpTo)
+            this.toNum = parseFloat(fromN) * parseFloat(rate);
+        },
         async getRate (from, to) {
             return await axios(this.API)
                 .then( response => response.data)
